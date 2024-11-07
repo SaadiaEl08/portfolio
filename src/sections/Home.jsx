b212f1dae127dec7c0ef7edfa620f9f241ba01f7
SaadiaEl08 <saadiaelachguir@gmail.com>
2024-11-07 13:23:28 +0100

    add redux

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,7 +1,40 @@
-const Home = () => {
-  return (
-    <div className="min-h-screen border">HomeHomeHomeH</div>
-  )
-}
+import { useState } from "react";
+import { jobs } from "../constants";
+import { useEffect } from "react";
+import icon from '../assets/morocco.svg';
 
-export default Home
+const Home = () => {
+  const [jobIndex, setJobIndex] = useState(0);
+  const downloadCv=()=>{
+    const link = document.createElement('a');
+    link.href = 'src/assets/Saadia_El_Achguir_CV.pdf';
+    link.download = 'Saadia_El_Achguir_CV.pdf';
+    link.click();
+  }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setJobIndex((prevIndex) => (prevIndex + 1) % jobs.length);
+    }, 2000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="min-h-screen min-w-screen flex flex-col justify-center items-center  p-2">
+      <div className="flex flex-col justify-center items-center gap-3">
+        <img src="src/assets/personal_photo.jpg" alt="personal photo" className="w-40  rounded-full  img-drop-shadow shadow-[var(--shadow)] " />
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-lg font-bold">Hello <i className="fa-solid fa-hand text-yellow-400 animate-wave"></i></h1>
+          <p className="text-wrap text-lg text-center">
+            I am <span className="text-blue-300 font-bold">Saadia</span>, a
+            <span className="font-bold text-blue-300 ">{jobs[jobIndex]}</span> Developer.
+            based in Morocco
+            <span className="ms-1" style={{ display: 'inline-block', backgroundImage: `url(${icon})`, width: '15px', height: '15px', backgroundSize: 'contain', backgroundRepeat: 'no-repeat', verticalAlign: 'middle' }} />
+          </p>
+        </div>
+        <button onClick={downloadCv} className="CTA-btn">CV/Resume <i className="fa-solid fa-download "></i></button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
