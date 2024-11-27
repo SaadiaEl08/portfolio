0c63ce6126d2ba0fdaf16b7f656b10c5b2b92489
SaadiaEl08 <saadiaelachguir@gmail.com>
2024-11-28 00:12:21 +0100

    adjust input text size for small devices

--- frontend/src/components/ContactFormElement.jsx
+++ frontend/src/components/ContactFormElement.jsx
@@ -4,7 +4,7 @@ const ContactFormElement = ({ label, icon, errMessage = "" ,...props}) => {
         <div className="flex flex-col gap-1 flex-nowrap">
             <label htmlFor={label}>{label}</label>
             <div className="flex items-center justify-start">
-                <input className="p-2 rounded-s-md outline-none text-[var(--input-text-color)] w-[89%] bg-[var(--input-background)]"  id={label}  {...props} />
+                <input className="p-2 rounded-s-md text-xs sm:text-base outline-none text-[var(--input-text-color)] w-[89%] bg-[var(--input-background)]"  id={label}  {...props} />
                 <div className="p-2 rounded-e-md bg-[var(--background-color)] ">
                     <i className={`fa-solid fa-${icon}`} ></i>
                 </div>
