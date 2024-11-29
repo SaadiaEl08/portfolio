b5743ae1d8a7144cd8b399eecfcaa68326ba5a64
SaadiaEl08 <saadiaelachguir@gmail.com>
2024-11-29 20:22:07 +0100

    try fixing hover error on phones

--- frontend/src/sections/NavBar.jsx
+++ frontend/src/sections/NavBar.jsx
@@ -16,6 +16,19 @@ const NavBar = () => {
   const dispatch = useDispatch();
   const [showLanguagesDropdown, setShowLanguagesDropdown] = useState(false);
 
+  const navElements = document.querySelectorAll('.navItem');
+  // Handle click event with timeout(fix hovering issue for mobile)
+  Array.from(navElements).map(item => item.addEventListener('click', () => {
+    item.classList.add('overflow-visible'); // Show overflow
+
+    // Automatically hide overflow after 0.5s
+     setTimeout(() => {
+      item.classList.remove('overflow-visible');
+    }, 500); // 500ms
+
+
+  }));
+
   useEffect(() => {
     window.addEventListener("click", (e) => {
       const hasClass = e.target.classList.contains("language-button");
@@ -56,7 +69,7 @@ const NavBar = () => {
     <nav className="border-2 rounded-full bg-[var(--background-color)] w-fit h-fit flex flex-col justify-center items-center p-1 z-10">
       <ul className="flex sm:flex-col gap-2 justify-evenly items-center w-72 sm:w-fit sm:px-1 md:h-[320px] md:p-0">
         {navItems.map((item) => (
-          <li key={item.id} className='flex flex-col sm:flex-row gap-6 sm:gap-4 justify-start items-center w-5  md:w-8 overflow-hidden hover:overflow-visible  h-8 sm:h-fit'>
+          <li key={item.id} className='navItem flex flex-col sm:flex-row gap-6 sm:gap-4 justify-start items-center w-5  md:w-8 overflow-hidden hover:overflow-visible   h-8 sm:h-fit'>
             <Link to={item.path} className='flex flex-col justify-center items-center  '>
               <i className={`${item.icon} w-5 text-center md:w-8 text-lg md:text-2xl  `} />
               {currentPage == item.path ? <span className='w-5 border bg-white md:w-6 md:border-1'></span> : ''}
@@ -67,9 +80,9 @@ const NavBar = () => {
         <li className='relative flex  flex-col sm:flex-row gap-6 sm:gap-4 justify-start items-center w-5  md:w-8 h-8 sm:h-fit '>
           <button className='w-full  flex flex-row justify-center items-center language-button' onClick={toggleLanguagesDropdown} >
             <i className={`fa-solid fa-globe w-1 text-center   text-lg md:text-2xl language-button `} />
-            <span className='text-xs text-center align-center font-bold language-button text-black'>{currentLanguage.code.toUpperCase()}</span>
+            <span className='text-xs text-center align-center font-bold language-button text-black'>{currentLanguage.code?.toUpperCase()}</span>
           </button>
-          <ul className={`gap-1 ${showLanguagesDropdown ? "flex" : "hidden" } absolute top-12 sm:top-4  ${currentLanguageCode=="ar"?" sm:-left-28" :"sm:left-10"} flex-col  justify-center items-center border-2 rounded-md bg-[var(--background-color)]`}>
+          <ul className={`gap-1 ${showLanguagesDropdown ? "flex" : "hidden"} absolute top-12 sm:top-4  ${currentLanguageCode == "ar" ? " sm:-left-28" : "sm:left-10"} flex-col  justify-center items-center border-2 rounded-md bg-[var(--background-color)]`}>
             {
               languages.map((language, index) => {
                 return (
