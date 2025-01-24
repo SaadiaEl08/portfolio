3c319175fa5b5abb62211f596edec7b73ae80d31
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-01-24 19:31:13 +0100

    feat: add index file to export main components for easier imports

---
+++ frontend/src/sections/index.js
@@ -0,0 +1,8 @@
+import About from "./About";
+import Home from "./Home";
+import Contact from "./Contact";
+import CV from "./CV";
+import NavBar from "./NavBar";
+
+
+export { CV, Contact, NavBar, About, Home };
