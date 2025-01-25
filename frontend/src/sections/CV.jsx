d7c830e227e4e24da494f24e4f85b9c8d37d01b3
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-01-25 02:42:46 +0100

    feat: refactor CV section to use forwardRef and improve accessibility

--- frontend/src/sections/CV.jsx
+++ frontend/src/sections/CV.jsx
@@ -1,19 +1,31 @@
 import { useTranslation } from "react-i18next";
 import HeroHeader from "../components/HeroHeader";
 import PersonalImg from "../components/PersonalImg";
-import DownloadCvButton from '../components/DownloadCvButton';
+import DownloadCvButton from "../components/DownloadCvButton";
+import { forwardRef } from "react";
 
-const CV = () => {
-    const { t } = useTranslation();
+const Cv = forwardRef((props,ref) => {
+  const { t } = useTranslation();
 
-    return (
-        <div id="cv/resume" className="flex flex-col justify-center items-center gap-2 sm:min-h-screen  md:text-lg 2xl:gap-6 p-2 animation-fade-in">
-            <PersonalImg className="w-[70px] md:w-[150px] lg:w-[100px] xl:w-[150px] 2xl:w-[300px] " />
-            <HeroHeader text={t("cv/resume")} />
-            <p className="text-lg w-[100%] sm:w-[80%] text-center">{t("cvText1")}<br/>{t("cvText2")}<br/><strong>{t("note")}</strong> {t("cvText3")} </p>
-            <DownloadCvButton />
-        </div>
-    );
-};
+  return (
+    <div
+      ref={ref}
+      id="cv/resume"
+      className="snap-start flex flex-col justify-center items-center gap-2 sm:min-h-screen  md:text-lg 2xl:gap-6 sm:p-2 pt-16 animation-fade-in"
+    >
+      <PersonalImg className="w-[70px] md:w-[150px] lg:w-[100px] xl:w-[150px] 2xl:w-[300px] " />
+      <HeroHeader text={t("cv/resume")} />
+      <p className="text-lg w-[100%] sm:w-[80%] text-center">
+        {t("cvText1")}
+        <br />
+        {t("cvText2")}
+        <br />
+        <strong>{t("note")}</strong> {t("cvText3")}{" "}
+      </p>
+      <DownloadCvButton />
+    </div>
+  );
+});
+Cv.displayName = "CV";
 
-export default CV;
+export default Cv;
