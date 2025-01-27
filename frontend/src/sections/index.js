5483986f7a532978a9c18f0a105fb810e05d6366
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-01-27 23:28:03 +0100

    feat: add Projects section to index export for improved navigation

--- frontend/src/sections/index.js
+++ frontend/src/sections/index.js
@@ -3,6 +3,7 @@ import Home from "./Home";
 import Contact from "./Contact";
 import Resume from "./Resume";
 import NavBar from "./NavBar";
+import Projects from "./Projects";
 
 
-export { Resume, Contact, NavBar, About, Home };
+export { Resume, Contact, NavBar, About, Home,Projects };
